0ef68674710fbbc8132506afd12a45d226f27b24
Quentin <quentin.senelas@gmail.com>
2019-05-15 17:05:02 +0200

    Fix mise en page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,79 +1,85 @@
 <!DOCTYPE html>
+
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Benoit Rocher - Mentions légales</title>
     <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <title>Document</title>
-</head>
-<body>
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+  </head>
 
-        <div class="container">
-          <!-- Header -->
-                <div class="headerr">
-                  <header>
-                      <h1 class="titre">
-                        <div class="logo">
-                        <a href="Accueil.blade.php">Ben Roc</a>
-                      </div>
-                      </h1>
-                
-                      <nav>
-                        <a href="Accueil.blade.php">Accueil</a>
-                        <br>
-                        <a href="photo.html">Photos</a>
-                        <br>
-                        <a href="prestations.html">Prestations</a>
-                        <br>
-                        <a href="contact.blade.php">Contact</a>
-                      </nav>
-                    </header>
+  <body>
+   
+      <div class="container">
+        <!-- Header -->
+        <div class="headerr">
+          <header>
+              <h1 class="titre">
+                <div class="logo">
+                <a href="Accueil.blade.php">Ben Roc</a>
               </div>
-              <!-- Formulaire-->
-                <div class="formulaire">
-                    <div class="nom">
-                    <p>Nom :</p><br />
-                    <input type="text" name="nom" value="" />
-                    </div>
-                    <div class="prenom">
-                    <p>Prénom :</p>
-                    <input type="text" name="prenom" value="" />
-                    </div>
-                    <div class="email">
-                    <p>E-mail :</p>
-                    <input type="text" name="email" value="" />
-                    </div>
-                    <div class="objet">
-                    <p>Objet :</p>
-                    <input type="text" name="objet" value="" />
-                    </div>
-                    <div class="message">
-                    <p>Message :</p>
-                    <input type="text" name="message" value="" />
-                    </div>    
-                </div>
+              </h1>
+        
+              <nav>
+                <a href="Accueil.blade.php">Accueil</a>
+                <br>
+                <a href="photo.html">Photos</a>
+                <br>
+                <a href="prestations.html">Prestations</a>
+                <br>
+                <a href="contact.blade.php">Contact</a>
+              </nav>
+            </header>
+      </div>
+        <!-- Formulaire -->
+        <div class="formulaire">
+          <div class="nom">
+            <label for="usr">Nom: </label>
+          <input type="text" name="nom" value=""> <br> <br> <br>
+          </div>
+          <div class="prenom">
+            <label for="usr">Prénom: </label>
+          <input type="text" name="prenom" value=""> <br> <br> <br>
+          </div>
+          <div class="email">
+            <label for="usr">E-mail: </label>
+          <input type="text" name="email" value=""> <br> <br> <br>
+          </div>
+          <div class="objet">
+            <label for="usr">Objet: </label>
+          <input type="text" name="objet" value=""> <br> <br> <br>
+          </div>
+          <div class="message">
+            <label for="usr">Message: </label>
+          <textarea type="text" name="message" value=""></textarea> <br> <br> <br>
+          </div>
+          <button type="button">Envoyer</button>
+      </div>
 
-              
 
-              <!-- Footer -->    
-              <footer>
-                    <div class="detail">
-          
-                      <div class="mention">
-                        <a href="mentions.blade.php" target="_blank">Mentions légales</a>
-                      </div>
-          
-                      <div class="info">
-                        <p>06.99.59.75.02 - benoit.roc@gmail.com - <a href="https://www.facebook.com/benroc.photographe" target="_blank">facebook</a></p>
-                        <p>Tous droits Réservé - 2019</p>
-                      </div>
-          
-                    </div>
-          
-                  </footer>
-          
-                </div>              
-                <script src="/js/jquery-3.3.1.min.js"></script>
-                <script type="text/javascript" src="/js/sliderpoo.js"></script>
-</body>
+         <!-- Footer -->
+        <footer>
+          <div class="detail">
+
+            <div class="mention">
+              <a href="mentions.blade.php" target="_blank">Mentions légales</a>
+            </div>
+
+            <div class="info">
+              <p>06.99.59.75.02 - benoit.roc@gmail.com - <a href="https://www.facebook.com/benroc.photographe" target="_blank">facebook</a></p>
+              <p>Tous droits Réservé - 2019</p>
+            </div>
+
+          </div>
+
+        </footer>
+
+      </div> 
+    
+      <script src="/js/jquery-3.3.1.min.js"></script>
+      <script type="text/javascript" src="/js/sliderpoo.js"></script>
+
+  </body>
 </html>
