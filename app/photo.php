8e8f30b0547b9af0dc6edbd57929d43c0bedeb55
camille <camillechassaing.jdb@gmail.com>
2019-05-21 16:57:42 +0200

    changement syntaxe Min / Max

--- app/photo.php
+++ app/photo.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class photo extends Model
+class Photo extends Model
 {
     public function categories_id(){
         return $this->belongsTo(categories::class);
