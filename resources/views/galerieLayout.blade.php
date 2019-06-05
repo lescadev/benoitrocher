90967745edba7da4b2a18ee11e0c538c4308a7ed
Jason <gjtoison8@gmail.com>
2019-06-05 16:48:53 +0200

    fix conflit

--- resources/views/galerieLayout.blade.php
+++ resources/views/galerieLayout.blade.php
@@ -8,6 +8,9 @@
 
 </div>
 <button class="afficher" type="button" class="titre" onclick="voir()">Tout Voir</button>
+<div class="infopresta">
+        @yield('descriptionpresta')
+</div>
 <!-- titres de contact -->
 <div class="contacttitre">
   <h2> Contactez-moi en remplissant le formulaire ci-dessous :</h2>
